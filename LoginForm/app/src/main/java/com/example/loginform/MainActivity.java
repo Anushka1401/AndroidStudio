a26315b813e3a5b45d466eb2cabfd77f9ae22c9b
Anushka Sanjeev <63353818+Anushka1401@users.noreply.github.com>
2023-01-05 14:10:33 +0530

    Add files via upload

---
+++ LoginForm/app/src/main/java/com/example/loginform/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.loginform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1;
+    EditText ed1, ed2;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1 = (Button)findViewById(R.id.button);
+        ed1 = (EditText)findViewById(R.id.edittext);
+        ed2 = (EditText)findViewById(R.id.edittext2);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ed1.getText().toString().equals("admin") &&
+                        ed2.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(),
+                            "Logging in...",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
+
+
+
+
+                }
+            }
+        });
+    }
+}
